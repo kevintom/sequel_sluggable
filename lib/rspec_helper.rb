a4d1f8e8f90a321bc253e07055f9de41f90d92d7
Pavel Kunc <pavel.kunc@gmail.com>
2009-11-17 15:29:01 +0000

    Add rspec macro to check sluggable behaviour in models.

---
+++ lib/rspec_helper.rb
@@ -0,0 +1,23 @@
+module Sequel::Plugins::Sluggable::RSpecHelper
+  def it_should_behave_like_sluggable(klass)
+    it "should have slug when created" do
+      model = klass.make(klass.slug_source_column => 'Test String')
+      model.slug.should eql 'test-string'
+    end
+
+    it "should update slug when #{klass.slug_source_column} is updated" do
+      model = klass.make(klass.slug_source_column => 'Test String')
+      lambda { model.update(klass.slug_source_column => 'Test String Two') }.should change(model,:slug).to('test-string-two')
+    end
+
+    it "should find #{klass} by it's ID" do
+      model = klass.make(klass.slug_source_column => 'Test String')
+      klass.find_by_id_or_slug(model.id).should eql model
+    end
+
+    it "should find #{klass} by it's slug" do
+      model = klass.make(klass.slug_source_column => 'Test String')
+      klass.find_by_id_or_slug('test-string').should eql model
+    end
+  end
+end
